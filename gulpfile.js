a847bd50029a1ed753c5d97d5f86d5c5c7559279
damienbrugiere <damien.brugiere.btssio@gmail.fr>
2016-03-19 18:20:48 +0100

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var  browserify = require('browserify');
+var  source     = require('vinyl-source-stream');
+
+// Static Server + watching scss/html files
+gulp.task('serve', function() {
+
+    browserSync.init({
+        server: "./app"
+    });
+    gulp.watch("app/**").on('change',['bundle','browserify', browserSync.reload]);
+});
+
+gulp.task('browserify', function() {
+    return browserify('./app/js/app.js')
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('./app/dist'));
+});
+
+gulp.task('default', ['serve']);
